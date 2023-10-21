963a9a4abaad26cbffb0d94d4ecece46a7aeaebe
Jozef Izso <izso@cisco.com>
2023-10-21 13:59:49 +0200

    Add `SUSignatures` class

---
+++ Sources/Appcast/SUSignatures.swift
@@ -0,0 +1,14 @@
+//
+// Copyright 2023 Cisco Systems, Inc. All rights reserved.
+// Licensed under MIT-style license (see LICENSE.txt file).
+//
+// Based on SUSignatures.m from Sparkle project.
+//
+
+public struct SUSignatures {
+    public let ed25519: String
+
+    public init(ed ed25519: String) {
+        self.ed25519 = ed25519
+    }
+}
