f3fc50009dc1115cbf9006b5cb32651e9157661a
Jozef Izso <izso@cisco.com>
2023-10-27 10:01:06 +0200

    Refactor integration test for custom namespace names

--- Tests/IntegrationTests/SUAppcastParserNamespacesTests.swift
+++ Tests/IntegrationTests/SUAppcastParserNamespacesTests.swift
@@ -6,25 +6,48 @@ import XCTest
 @testable import Appcast
 
 class SUAppcastParserNamespacesTest: XCTestCase {
-    func testNamespaces() {
-        let testFile = Bundle.module.path(forResource: "testnamespaces", ofType: "xml")!
-        let testData = NSData(contentsOfFile: testFile)!
-
-        do {
-            let stateResolver = SPUAppcastItemStateResolver(hostVersion: "1.0", applicationVersionComparator: SUStandardVersionComparator.default, standardVersionComparator: SUStandardVersionComparator.default)
-            
-            let appcast = try SUAppcast(xmlData: testData as Data, relativeTo: nil, stateResolver: stateResolver)
-            let items = appcast.items
-
-            XCTAssertEqual(2, items.count)
-
-            XCTAssertEqual("Version 2.0", items[1].title)
-            XCTAssertEqual("desc", items[1].itemDescription)
-            XCTAssertNotNil(items[0].releaseNotesURL)
-            XCTAssertEqual("https://sparkle-project.org/#works", items[0].releaseNotesURL!.absoluteString)
-        } catch let err as NSError {
-            NSLog("%@", err)
-            XCTFail(err.localizedDescription)
+    var appcast = SUAppcast.empty
+    
+    override func setUpWithError() throws {
+        guard let testURL = Bundle.module.url(forResource: "testnamespaces", withExtension: "xml") else {
+            fatalError("Resource file is not bundled in integration test.")
         }
+        
+        let testData = try Data(contentsOf: testURL)
+        
+        let versionComparator = SUStandardVersionComparator.default
+        let hostVersion = "1.0"
+        let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: versionComparator, standardVersionComparator: versionComparator)
+        
+        self.appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
+    }
+    
+    func test_init_appcastWithCustomNamespaceName_count() {
+        // Arrange
+        let actualCount = self.appcast.items.count
+
+        // Assert
+        XCTAssertEqual(2, actualCount)
+    }
+    
+    func test_init_appcastWithCustomNamespaceName_version2() {
+        // Arrange
+        let actualItemVersion2 = self.appcast.items[1]
+
+        // Assert
+        XCTAssertEqual("Version 2.0", actualItemVersion2.title)
+        XCTAssertEqual("desc", actualItemVersion2.itemDescription)
+        XCTAssertNil(actualItemVersion2.releaseNotesURL)
+    }
+    
+    func test_init_appcastWithCustomNamespaceName_version3() {
+        // Arrange
+        let actualItemVersion3 = self.appcast.items[0]
+
+        // Assert
+        XCTAssertEqual("Version 3.0", actualItemVersion3.title)
+        XCTAssertNil(actualItemVersion3.itemDescription)
+        XCTAssertNotNil(actualItemVersion3.releaseNotesURL)
+        XCTAssertEqual("https://sparkle-project.org/#works", actualItemVersion3.releaseNotesURL?.absoluteString)
     }
 }
