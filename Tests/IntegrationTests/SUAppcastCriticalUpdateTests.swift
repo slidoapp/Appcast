b7d60637305eef938d4d9b5babd591f78b2e9ac7
Jozef Izso <izso@cisco.com>
2023-11-05 14:22:49 +0100

    Refactor integration tests for appcast with critical updates

--- Tests/IntegrationTests/SUAppcastCriticalUpdateTests.swift
+++ Tests/IntegrationTests/SUAppcastCriticalUpdateTests.swift
@@ -6,43 +6,56 @@ import XCTest
 @testable import Appcast
 
 class SUAppcastCriticalUpdateTests: XCTestCase {
-    func testCriticalUpdateVersion() {
-        let testURL = Bundle.module.url(forResource: "testappcast", withExtension: "xml")!
-        
-        do {
-            let testData = try Data(contentsOf: testURL)
-            
-            let versionComparator = SUStandardVersionComparator.default
-            
-            // If critical update version is 1.5 and host version is 1.0, update should be marked critical
-            do {
-                let hostVersion = "1.0"
-                let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: versionComparator, standardVersionComparator: versionComparator)
-                
-                let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
-                XCTAssertTrue(appcast.items[0].isCriticalUpdate)
-            }
-            
-            // If critical update version is 1.5 and host version is 1.5, update should not be marked critical
-            do {
-                let hostVersion = "1.5"
-                let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: versionComparator, standardVersionComparator: versionComparator)
-                
-                let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
-                XCTAssertFalse(appcast.items[0].isCriticalUpdate)
-            }
-            
-            // If critical update version is 1.5 and host version is 1.6, update should not be marked critical
-            do {
-                let hostVersion = "1.6"
-                let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: versionComparator, standardVersionComparator: versionComparator)
-                
-                let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
-                XCTAssertFalse(appcast.items[0].isCriticalUpdate)
-            }
-        } catch let err as NSError {
-            NSLog("%@", err)
-            XCTFail(err.localizedDescription)
+    let versionComparator = SUStandardVersionComparator.default
+    var testData = Data()
+    
+    override func setUpWithError() throws {
+        guard let testURL = Bundle.module.url(forResource: "testappcast", withExtension: "xml") else {
+            fatalError("Resource file is not bundled in integration test.")
         }
+        
+        self.testData = try Data(contentsOf: testURL)
+    }
+    
+    /// If critical update version is 1.5 and host version is 1.0, update should be marked critical/
+    func test_isCriticalUpdate_hostVersionIsLowerThanCriticalUpdateRelase() throws {
+        // Arrange
+        let hostVersion = "1.0"
+        let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: self.versionComparator, standardVersionComparator: self.versionComparator)
+        
+        // Act
+        let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
+        let actualItem = appcast.items[0]
+        
+        // Assert
+        XCTAssertTrue(actualItem.isCriticalUpdate)
+    }
+
+    /// If critical update version is 1.5 and host version is 1.5, update should not be marked critical/
+    func test_isCriticalUpdate_hostVersionIsSameAsCriticalUpdateRelase() throws {
+        // Arrange
+        let hostVersion = "1.5"
+        let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: self.versionComparator, standardVersionComparator: self.versionComparator)
+        
+        // Act
+        let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
+        let actualItem = appcast.items[0]
+        
+        // Assert
+        XCTAssertFalse(actualItem.isCriticalUpdate)
+    }
+    
+    /// If critical update version is 1.5 and host version is 1.6, update should not be marked critical/
+    func test_isCriticalUpdate_hostVersionIsHigherThanCriticalUpdateRelase() throws {
+        // Arrange
+        let hostVersion = "1.6"
+        let stateResolver = SPUAppcastItemStateResolver(hostVersion: hostVersion, applicationVersionComparator: self.versionComparator, standardVersionComparator: self.versionComparator)
+        
+        // Act
+        let appcast = try SUAppcast(xmlData: testData, relativeTo: nil, stateResolver: stateResolver)
+        let actualItem = appcast.items[0]
+        
+        // Assert
+        XCTAssertFalse(actualItem.isCriticalUpdate)
     }
 }
