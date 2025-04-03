5fe8d74448813452284b94d287c56fa70d1b58bb
Jozef Izso <izso@cisco.com>
2025-04-03 13:52:25 +0200

    Make `SUOperatingSystem` struct sendable

--- Tests/AppcastTests/SUOperatingSystemTests.swift
+++ Tests/AppcastTests/SUOperatingSystemTests.swift
@@ -11,12 +11,25 @@ class SUOperatingSystemTests: XCTestCase {
     func test_systemVersionString_matchesExpectedFormat() throws {
         // Arrange
         let expectedFormat = try NSRegularExpression.init(pattern: "\\d+.\\d+.\\d+")
+        let osVersion = SUOperatingSystem()
 
         // Act
-        let actualVersionString = SUOperatingSystem.systemVersionString
+        let actualVersionString = osVersion.systemVersionString
         let actualMatchCount = expectedFormat.numberOfMatches(in: actualVersionString, range: NSMakeRange(0, actualVersionString.count))
         
         // Assert
         XCTAssertEqual(actualMatchCount, 1)
+    }
+    
+    func test_systemVersionString_matchesProvidedOperatingSystemVersionValue() throws {
+        // Arrange
+        let mockVersion = OperatingSystemVersion(majorVersion: 15, minorVersion: 3, patchVersion: 2)
+        let osVersion = SUOperatingSystem(mockVersion)
+
+        // Act
+        let actualVersionString = osVersion.systemVersionString
+        
+        // Assert
+        XCTAssertEqual(actualVersionString, "15.3.2")
     }
 }
