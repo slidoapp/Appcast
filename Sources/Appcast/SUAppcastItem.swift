5dc9932f57a53978268ffcbf737c1d6d0b7a11d8
Jozef Izso <izso@cisco.com>
2025-05-18 23:49:04 +0200

    Fix parsing of the `<description sparkle:format="">` attribute value
    Fixes Appcast item description format is not parsed correctly #10
    Fixes the test `IntegrationTests.SUAppcastParserTest test_items_item0`

--- Sources/Appcast/SUAppcastItem.swift
+++ Sources/Appcast/SUAppcastItem.swift
@@ -524,7 +524,7 @@ public struct SUAppcastItem: Sendable, Equatable {
         let descriptionDict = dict[SURSSElement.Description] as? SUAppcast.AttributesDictionary
         
         self.itemDescription = descriptionDict?["content"] as? String
-        self.itemDescriptionFormat = descriptionDict?["format"] as? String ?? self.itemDescription != nil ? "html" : nil
+        self.itemDescriptionFormat = descriptionDict?["format"] as? String ?? (self.itemDescription != nil ? "html" : nil)
         
         if let infoUrlString = dict[SURSSElement.Link] as? String {
             guard let infoUrl = URL(string: infoUrlString, relativeTo: appcastURL) else {
